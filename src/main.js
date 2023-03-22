b005799d96110600d6a07b23d651967073b1af5c
Severino Vinicius <severino.sales95@gmail.com>
2023-03-22 20:17:11 -0300

    Adicionando Requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const products = document.querySelector('.products');
+const getProducts = await fetchProductsList('computador');
+
+const listOfItens = () => getProducts
+  .forEach((element) => products.appendChild(createProductElement({
+    id: element.id,
+    title: element.title,
+    thumbnail: element.thumbnail,
+    price: element.price,
+  })));
+
+listOfItens();
